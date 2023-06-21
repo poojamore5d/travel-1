3ced5d9e2da04852cc2e097492f772491afce0e1
poojamore5d <124284213+poojamore5d@users.noreply.github.com>
2023-06-21 12:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
-model=pickle.load(open('Model.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
 @app.route('/')
 def home():
     #return 'Hello World'
